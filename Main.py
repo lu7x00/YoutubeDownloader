9a55109d11d3ee27bbd795662fe3cafcbe9bebcf
Lu7x00 <lu7x00@protonmail.com>
2019-11-30 17:43:17 +0100

    Add video with audio function

--- Main.py
+++ Main.py
@@ -26,14 +26,14 @@ print(yt.title)
 folderName = input("Enter name of folder with your audio/video: ")
 os.mkdir(folderName)
 
-print("(1) Audio\t(2) Video\nOption:", end=" ")
+print("(1) Audio\t(2) Video\t(3) Audio-Video\nOption:", end=" ")
 choice = int(input())
 
 if choice == 1:
     stream = yt.streams.get_by_itag('140')
     stream.download("./" + folderName)
 elif choice == 2:
-    print("Quality: \n (144) 144p\n(240) 240p\n(360) 360p\n(480) 480p\n(720) 720p\n(1080) 1080p")
+    print("Quality: \n(144) 144p\n(240) 240p\n(360) 360p\n(480) 480p\n(720) 720p\n(1080) 1080p")
     quality = int(input("Quality of Video: "))
     if quality == 144:
         stream = yt.streams.get_by_itag('160')
@@ -52,6 +52,16 @@ elif choice == 2:
         stream.download("./" + folderName)
     if quality == 1080:
         stream = yt.streams.get_by_itag('137')
+        stream.download("./" + folderName)
+elif choice == 3:
+    print("Quality: \n(360) 360p\n(720) 720p")
+    print("Quality of audio/video: ", end="")
+    quality = int(input())
+    if quality == 360:
+        stream = yt.streams.get_by_itag('18')
+        stream.download("./" + folderName)
+    if quality == 720:
+        stream = yt.streams.get_by_itag('22')
         stream.download("./" + folderName)
 
 
